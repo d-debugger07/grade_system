ea8534978bffcfd96a7cc040d6b81f241f5eeaff
d-debugger07 <usmanabdulrahmanx007@gmail.com>
2025-10-02 08:10:46 +0100

    Add files via upload

---
+++ grade_system.py
@@ -0,0 +1,17 @@
+score=int(input("what is your score"))
+if score >=70 and score <=100:
+    print(f"Grade A")
+elif score >=60 and score <=69:
+    print(f"grade B")
+
+elif score >=50 and score <=59:
+    print(f"grade C")
+
+elif score >=45 and score <=49:
+    print (f"Grade D")
+
+elif score >=40 and Score <=44:
+    print (f"grade E")
+
+elif score >=0 and score <=30:
+    print (f"grade F")
